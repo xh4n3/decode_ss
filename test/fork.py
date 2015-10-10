0a17518c322241f29a9e5bbf653cac146ddf2328
Xh4n3 <xyn1016@gmail.com>
2015-10-10 18:23:31 +0800

    fork

--- test/fork.py
+++ test/fork.py
@@ -1,23 +1,26 @@
 import time
+import os
 
 __author__ = 'Sh4n3'
 
-import os
-
 
 def pp(pid):
-    while True:
+    for i in range(3):
         time.sleep(1)
         print 'pid %s' % str(os.getpid())
 
 for i in range(3):
     p = os.fork()
 
+children = []
+
 if p == 0:
     pp(1)
-
-while True:
-    time.sleep(10)
-    print os.getpid()
-
+else:
+    # TODO WTF HOW DOES IT HAPPEN
+    children.append(p)
+    print children
+    for child in children:
+        os.waitpid(child, 0)
+        print 'wait end'
 
